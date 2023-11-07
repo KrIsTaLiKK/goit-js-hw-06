dd0ba03986f82fc907c1f85432f01eae7ec92249
Krystyna Zolotar <zkristal95@gmail.com>
2023-11-07 14:57:12 -0300

    corrected 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -11,24 +11,12 @@ const refs = {
   elContainer: document.querySelector("#boxes"),
 };
 
-// refs.input.addEventListener("change", getValueInput);
-refs.btnCreateEl.addEventListener("click", onCreateBtnClick);
+refs.btnCreateEl.addEventListener("click", createBoxes);
 refs.btnDestroyEl.addEventListener("click", destroyBoxes);
 
-// --------------------------------------
-
-// function getValueInput(event) {
-//   return Number(event.currentTarget.value);
-// }
-
-function onCreateBtnClick() {
-  const inputValue = refs.input.value;
-  createBoxes(inputValue);
-}
-
-function createBoxes(amount) {
+function createBoxes() {
+  const amount = refs.input.value;
   let boxesMarkup = "";
-
   for (let i = 0; i < amount; i += 1) {
     let size = 30 + 10 * i;
     const bgColor = getRandomHexColor();
@@ -44,58 +32,3 @@ function destroyBoxes() {
   refs.elContainer.innerHTML = "";
   refs.input.value = "";
 }
-// -----------------------------------------
-// function createBoxes(amount) {
-//   const arrDivEl = [];
-//   let basicFontSize = 30;
-//   let step = 10;
-
-//   for (let i = 0; i < amount; i += 1) {
-//     const divEl = document.createElement("div");
-//     divEl.style.width = `${basicFontSize + step * i}px`;
-//     divEl.style.height = `${basicFontSize + step * i}px`;
-//     divEl.style.backgroundColor = getRandomHexColor();
-//     arrDivEl.push(divEl);
-//   }
-
-//   refs.elContainer.append(...arrDivEl);
-// }
-
-// function onCreateBtnClick() {
-//   let inputValue = Number(refs.input.value);
-//   createBoxes(inputValue);
-// }
-
-// function destroyBoxes() {
-//   refs.elContainer.innerHTML = "";
-//   refs.input.value = "";
-// }
-// kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
-
-// const inputRef = document.querySelector("#controls input");
-// const createButtonRef = document.querySelector("button[data-create]");
-// const destroyButtonRef = document.querySelector("button[data-destroy]");
-// const boxesContainerRef = document.querySelector("#boxes");
-
-// createButtonRef.addEventListener("click", createBoxes);
-// destroyButtonRef.addEventListener("click", destroyBoxes);
-
-// function createBoxes() {
-//   const amount = inputRef.value;
-//   const boxes = [];
-
-//   for (let i = 0; i < amount; i += 1) {
-//     const box = document.createElement("div");
-//     const size = 30 + i * 10;
-//     box.style.width = `${size}px`;
-//     box.style.height = `${size}px`;
-//     box.style.backgroundColor = getRandomHexColor();
-//     boxes.push(box);
-//   }
-
-//   boxesContainerRef.append(...boxes);
-// }
-
-// function destroyBoxes() {
-//   boxesContainerRef.innerHTML = "";
-// }
